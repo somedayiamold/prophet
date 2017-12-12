f25af1ab42022c07f6b06b9239521ea61544a677
somedayiamold <luoyou_198117@163.com>
2017-12-12 09:46:21 +0800

    change views.py

--- prototype/views.py
+++ prototype/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'system': '测试系统', 'corporation': 'Hualala OP', 'wiki': 'Wiki'})
+    return render(request, 'index.html', {'system': '测试系统', 'corporation': 'XXXX', 'wiki': 'Wiki'})
